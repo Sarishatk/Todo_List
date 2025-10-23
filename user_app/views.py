ca53fa12e71e1bb516b039b8b83fe94f751203a2
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-23 08:27:45 +0530

    signup created

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.forms import UserRegistrationForm
 # Create your views here.
+
+
+class RegisterView(View):
+
+    def get(self,request):
+
+        form = UserRegistrationForm()
+
+        return render(request,"signup.html",{'form':form})
+
+
+

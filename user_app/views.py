e40df6501dc054620162bd2ae9d7108deb4afdbe
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-23 08:49:22 +0530

    post method compltetd

--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
 from user_app.forms import UserRegistrationForm
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 
@@ -16,9 +18,20 @@ class RegisterView(View):
 
         print(request.POST)
 
-        
+        username = request.POST.get('username')
+
+        first_name =  request.POST.get("first_name")
+
+        last_name =  request.POST.get("last_name")
+
+        password = request.POST.get("password")
+
+        email =  request.POST.get("email")
 
         form = UserRegistrationForm()
+
+        User.objects.create_user(username =username,first_name = first_name, last_name = last_name,password=password,email=email)
+
         return render(request,"signup.html",{'form':form})
 
 

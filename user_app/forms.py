430d81ccad7e53d3269b06613d966892316a305e
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-23 08:21:45 +0530

    TODO

---
+++ user_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+
+    class Meta:
+
+        model = User
+
+        feild = ['username','first_name','last_name','email','password']
+
+
+

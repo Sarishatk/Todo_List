ad8d1318805db50b515727a523faa753e6941a9f
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-29 20:30:20 +0530

    c17

--- todo_tracker/views.py
+++ todo_tracker/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import View
 from todo_tracker.forms import Todoform
+from todo_tracker.models import Todo_track
 
 
 
@@ -16,5 +17,30 @@ class addTodoList(View):
     
     def post(self,request):
 
+        print(request.POST)
+
+        form = Todoform(request.POST)
+
+        if form.is_valid():
+
+            print(form.cleaned_data)
+
+            todo_list = form.save(commit=False)
+
+            todo_list.user = request.user
+
+            todo_list.save()
+
+            return render(request,"add_task.html",{'form':form})
         
+
+class todolistview(View):
+
+    def get(self, request):
+
+        alllist = Todo_track.objects.filter(user = request.user)
+
+        return render(request,"list.html",{'alllist':alllist})
+
+
 

c2167110ff9ef025e6f686d077f1240823dbbb31
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-29 14:26:23 +0530

    dfef

---
+++ todo_tracker/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from todo_tracker.models import Todo_track
+
+class Todoform(forms.ModelForm):
+
+    class Meta:
+
+        model = Todo_track()
+
+        fields = ['category','titile','date','price','note']
+        
+
+
+
+    
+
+      

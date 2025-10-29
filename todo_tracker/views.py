b9b276966a3fe11ca71715b293038df90a0de9b2
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-29 13:34:52 +0530

    addTodoList

--- todo_tracker/views.py
+++ todo_tracker/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+from todo_tracker.models import Todo_track
+
+
+class addTodoList(View):
+
+    def get(self, request):
+
+        form = Todo_track()
+
+        return render(request,"add_task.html",{'form':form})
+
